61d7d8781a12dfce0e1cc79fee46be0475d31408
Xavi002 <diogomartins200214@gmail.com>
2020-08-05 23:37:40 +0100

    total connections

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+
 import ClassesController from './controllers/ClassesController';
 import ConnectionsController from './controllers/ConnectionsController';
 
@@ -9,6 +10,7 @@ const connectionsController = new ConnectionsController();
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
+routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
 
 export default routes;
